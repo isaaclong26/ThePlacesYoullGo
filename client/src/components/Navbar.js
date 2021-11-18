47d77bb657fd20059d26d4dffc25f72350f49a19
Molly Singmaster <mollysingmaster@gmail.com>
2021-11-18 14:08:15 -0500

    changes to the home structure

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,52 +1,44 @@
 import React from "react";
-import Nav from 'react-bootstrap/Nav';
-import Button from 'react-bootstrap/Button';
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
 import "./css/navbarstyle.css";
 
 export default function Header({ currentPage, handlePageChange }) {
   return (
     <Nav fixed="top" className="justify-content-center">
       <Nav.Item>
-            <Nav.Link
-              href="/"
-              onClick={() => handlePageChange("Home")}
-            >
-              Home
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link
-              href="#places"
-              onClick={() => handlePageChange("Places")}
-            >
-              Places
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link
-              href="#profile"
-              onClick={() => handlePageChange("Profile")}
-            >
-              Profile
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link
-              href="#bucketlist"
-              onClick={() => handlePageChange("Bucket List")}
-            >
-              Bucket List
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Button variant="outline-secondary"
-            className="button-login"
-              href="#login"
-              onClick={() => handlePageChange("Login")}
-            >
-              Login
-            </Button>
-          </Nav.Item>
+        <Nav.Link href="/" onClick={() => handlePageChange("Home")}>
+          Home
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="#places" onClick={() => handlePageChange("Places")}>
+          Places
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="#profile" onClick={() => handlePageChange("Profile")}>
+          Profile
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link
+          href="#bucketlist"
+          onClick={() => handlePageChange("Bucket List")}
+        >
+          Bucket List
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Button
+          variant="outline-secondary"
+          className="button-login"
+          href="#login"
+          onClick={() => handlePageChange("Login")}
+        >
+          Login
+        </Button>
+      </Nav.Item>
     </Nav>
   );
 }
