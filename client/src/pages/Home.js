ca4bca757c61dfed418bc3980ceb55f9f8e3b3f3
Molly Singmaster <mollysingmaster@gmail.com>
2021-11-18 10:29:13 -0500

    added the link to the home page and created a starter footer

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Carousel from "../components/Carousel";
+// import Carousel from "../components/Carousel/index.js";
 import { Container, Row, Col } from 'react-bootstrap';
 
 function Home() {
     return (
         <>
-            <Carousel />
+            {/* <Carousel /> */}
             <Container className="my-5">
                 <Row>
                     <Col data-aos="fade-up" className="m-4 default">
@@ -21,7 +21,7 @@ function Home() {
                         </ul>
                         <p>Prepare for your next getaway with The Places Youll Go!</p>
                         <hr />
-                        <h6>If you are just visiting or new to the site, onClick “Register” at the top of this page. If you have visited before and having already started planning your next vaycay, just “Log In” to continuing planning your trip.</h6>
+                        <h6>If you are just visiting or new to the site, Click “Register” at the top of this page. If you have visited before and having already started planning your next vaycay, just “Log In” to continuing planning your trip.</h6>
                     </Col >
                 </Row>
             </Container>
