be9fa997208759cfb7a0ae3108b8233e248aa4e5
Molly Singmaster <mollysingmaster@gmail.com>
2021-11-18 10:49:40 -0500

    added carousel to home page and adjusted folder structure in components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Home from './pages/Home';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Carousel from "./components/Carousel";
 
 function App() {
   return (
     <div>
+      <Navbar />
+      <Carousel />
       <Home />
       <Footer />
     </div>
--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./style.css"
+import "./Carousel_style.css"
 
 function Carousel(props) {
   return (
