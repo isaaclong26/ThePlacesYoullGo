3913ecc2f64b2e744f67f43762c07e5667aa4bf4
isaac long <isaaclong123@outlook.com>
2021-11-19 14:13:29 -0600

    fixed server bug

--- api/server.js
+++ api/server.js
@@ -1,23 +1,26 @@
 const express = require('express');
-const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
-const db = require('./config/connection');
+const path = require('path');
+require('dotenv').config()
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
-const app = express();
+const db = require('./config/connection');
+
 const PORT = process.env.PORT || 3001;
+const app = express();
+console.log(typeDefs)
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
-
-
 server.applyMiddleware({ app });
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
@@ -29,7 +32,6 @@ app.get('*', (req, res) => {
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
