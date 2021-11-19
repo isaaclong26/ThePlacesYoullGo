8cb94b07818927ca51c6499426513c548c3edcea
isaac long <isaaclong123@outlook.com>
2021-11-19 10:03:51 -0600

    finished authentification

---
+++ api/schemas/resolvers.js
@@ -0,0 +1,44 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Thought } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+
+    Query: {
+        users: ()=> {return User.findAll()}
+         },
+        user: async (parent, { username }) => {
+        return User.findOne({ username })
+         },
+  
+         Mutation: {
+            addUser: async (parent, { username, email, password }) => {
+              const user = await User.create({ username, email, password });
+              const token = signToken(user);
+              return { token, user };
+            },
+            login: async (parent, { email, password }) => {
+              const user = await User.findOne({ email });
+        
+              if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+              }
+        
+              const correctPw = await user.isCorrectPassword(password);
+        
+              if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+              }
+        
+              const token = signToken(user);
+        
+              return { token, user };
+            },
+         },
+
+}
+
+
+
+
+module.exports= resolvers
