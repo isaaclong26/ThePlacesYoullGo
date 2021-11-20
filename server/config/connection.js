4ff47fd44ce81b695a330580cc53c39131f24c22
Molly Singmaster <mollysingmaster@gmail.com>
2021-11-20 13:59:44 -0500

    added the mongodb link

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || process.env.MONGODB_URI,
+  process.env.MONGODB_URI || 'mongodb://localhost/placesyoullgo',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
