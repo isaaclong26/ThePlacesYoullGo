88f17ab43a3e3e07f629bf4431feacdfcc3e52bc
Kbentley8 <88289885+Kbentley8@users.noreply.github.com>
2021-11-20 12:44:55 -0500

    updated profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,9 +4,12 @@ import {Container, Row, Col, Form, Button} from 'react-bootstrap';
 class Profile extends React.Component {
     render() {
       return (
-        <div>
-          hello
-        </div>
+        <><div>
+          <h1> Username  </h1>
+        </div><p id="paragraph1"> Email: <br>
+        </br> Location: 
+        <p> Age:</p>
+          </p></> 
       );
     }
   
