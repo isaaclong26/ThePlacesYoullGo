2ced75c9b1110bca08de09c0ab07fcc853a92cab
isaac long <isaaclong123@outlook.com>
2021-11-20 15:44:18 -0600

    ode

--- client/src/App.js
+++ client/src/App.js
@@ -7,28 +7,76 @@ import Profile from './pages/Profile';
 import Places from './pages/Places';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Signup from './pages/Signup';
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 
 // import Login from "./components/LoginRoute";
 
 function App() {
   const [token, setToken] = useState();
   return (
+    <ApolloProvider client={client}>
     <div>
       <Router>
       <Navbar />
-      <Routes>
-      <Route exact path = "/" element={<Home />}/>
-      <Route exact path = "/login" element={<Login />}/>
-      <Route exact path = "/bucket" element={<Bucket />}/>
-      <Route exact path = "/profile" element={<Profile />}/>
-      <Route exact path = "/places" element={<Places />}/>
+    
+      <Route exact path = "/" >
+        <Home />
+      </Route>
+      <Route exact path = "/login" >
+      <Login />
+      </Route>
+      <Route exact path = "/signup" >
+      <Signup />
+      </Route>
+    
+      <Route exact path = "/bucket" >
+      <Bucket />
+      </Route>
+      <Route exact path = "/profile" >
+      <Profile />
+      </Route>
+      <Route exact path = "/places" >
+      <Places />
+      </Route>
 
-      </Routes>
+      
       <Footer />
       </Router>
     </div>
+    </ApolloProvider>
   );
 }
 
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,26 @@
 import { gql } from '@apollo/client';
 
-export const CREATE_LIST = gql`
-    mutation createList($item: String!) {
-        createList(item: $item)
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
-`
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
