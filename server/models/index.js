cbb4fbe6e7099afbdcfe54c4503b11f3969fafa5
isaac long <isaaclong123@outlook.com>
2021-11-21 10:29:34 -0600

    commit

--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,4 @@
 const User = require("./User")
 
 
-<<<<<<< HEAD:api/models/index.js
 module.exports= User
-=======
-module.exorts= { User };
->>>>>>> a4392143fc20804448890758239d329ba3ff6925:server/models/index.js
