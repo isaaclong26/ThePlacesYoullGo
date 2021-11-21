5172c986ccbbebd42d15e4fb9c11f6acfa5ebbab
Savion Elsey-Williams <s.e.williams2001@gmail.com>
2021-11-21 15:14:30 -0500

    added all seed data, not finished seeding

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,7 +1,8 @@
 const db = require('../config/connection');
-const { User, Bucket } = require('../models');
+const { User, Bucket, Places } = require('../models');
 const usersSeeds = require('./usersSeeds.json');
 const bucketSeeds = require('./bucketSeeds.json');
+const placesSeeds = require('./placesSeeds.json');
 
 const seedDatabase = async () => {
 
@@ -9,8 +10,12 @@ const seedDatabase = async () => {
     try {
       await User.deleteMany({});
       await User.create(usersSeeds);
+
       await Bucket.deleteMany({});
       await Bucket.create(bucketSeeds);
+
+      await Places.deleteMany({});
+      await Places.create(placesSeeds);
   
       console.log('all done!');
       process.exit(0);
